516c7ecd5ce837d21711cf22e8fdf4a801e69480
Reed La Botz <reedlabotz@gmail.com>
2012-02-03 01:22:35 -0600

    Basic visualization working.

--- Site/data.php
+++ Site/data.php
@@ -10,15 +10,15 @@ mysql_select_db($dbname);
 $machines = array();
 $data = array();
 
-$query = "SELECT machine_id,cpu_user,time FROM `snapshots`"; 
+$query = "SELECT machine_id,time,user_count FROM `snapshots`"; 
 $result =  mysql_query($query);
 while($row = mysql_fetch_array($result)){
-  $m_id = $row['machine_id'];
-  if(!array_key_exists($m_id,$machines)){
-    $machines[$m_id] = sizeof($data);
-    array_push($data,array());
-  }
-  array_push($data[$machines[$m_id]],array('x' => $row['time'], 'y' => $row['cpu_user']));
+   $m_id = $row['machine_id'];
+   if(!array_key_exists($m_id,$machines)){
+      $machines[$m_id] = sizeof($data);
+      array_push($data,array());
+   }
+   array_push($data[$machines[$m_id]],array('x' => strtotime($row['time'])*1000, 'y' => intval($row['user_count'])));
 }
 
 mysql_close($dbconn);
--- Site/js/stream.js
+++ Site/js/stream.js
@@ -1,15 +1,46 @@
 var width = 960;
-var height = 300;
+var height = 400;
 
-$.getJSON('data.php',function(data){
-  var data0 = d3.layout.stack().offset("wiggle")(data);
-  console.log(data0);
-  
-  var color = d3.interpolateRgb("#aad", "#556");
-  
-  var area = 
+d3.json('data.php', function(json){
+   var minX = d3.min(json,function(d){ return d3.min(d,function(d){ return d.x; }); });
+   var maxX = d3.max(json,function(d){ return d3.max(d,function(d){ return d.x; }); });
+   var maxY = d3.max(json,function(d){ return d3.max(d,function(d){ return d.y }); });
+   
+   var data0 = d3.layout.stack().offset("silhouette")(json);
 
-  var vis = d3.select("#chart").append("svg").attr("width",width).attr("height",height);
-  vis.selectAll("path").data(data0).enter().append("path").style("fill",function(){ return color(Math.random()); }).attr("d",area);
-});
+   var x = d3.time.scale().domain([new Date(minX), new Date(maxX)]).range([0, width]);
 
+   var y = d3.scale.linear().domain([0, maxY]).range([0, height/4]);
+
+   
+   var vis = d3.select("#chart").append("svg").attr("width",width).attr("height",height);
+   
+   var rules = vis.selectAll("g.rule")
+       .data(x.ticks(12))
+     .enter().append("g")
+       .attr("class", "rule");
+
+   rules.append("line")
+       .attr("x1", x)
+       .attr("x2", x)
+       .attr("y1", 0)
+       .attr("y2", height - 1);
+
+   rules.append("text")
+       .attr("x", x)
+       .attr("y", height - 13)
+       .attr("dy", ".71em")
+       .attr("text-anchor", "middle")
+       .text(x.tickFormat(12));
+   
+   var area = d3.svg.area()
+                    .x(function(d){ return x(new Date(d.x)); })
+                    .y0(function(d){ return y(d.y0); })
+                    .y1(function(d){ return y(d.y + d.y0); });
+
+   var color = d3.interpolateRgb("#aad", "#556");
+
+   var i = 0;
+   vis.selectAll("path").data(data0).enter().append("path").style("fill",function(){ return color(i++/4); }).attr("d",area);
+   
+  });
