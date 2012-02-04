e20d2c0af44c2e420aa36cd074f1b2ca130ada08
Reed La Botz <reedlabotz@gmail.com>
2012-02-03 23:39:12 -0600

    Better sql select statement

--- Site/json.php
+++ Site/json.php
@@ -27,8 +27,8 @@ if($request == "data"){
       $start = date("Y-m-d H:i:s",mysql_real_escape_string($_GET['start'])/1000);
       $end = date("Y-m-d H:i:s",mysql_real_escape_string($_GET['end'])/1000);
       $machines = array();
-      $query = "SELECT machine_id,time,".$var." FROM `snapshots` JOIN `machines` ON `snapshots`.machine_id=`machines`.id ";
-      $query .="WHERE `machines`.active=1 AND `snapshots`.time >= '".$start."' AND `snapshots`.time <= '".$end."' ORDER BY `snapshots`.machine_id,`snapshots`.time"; 
+      $query = "SELECT machine_id,time,".$var." FROM `snapshots`";
+      $query .="WHERE time >= '".$start."' AND time <= '".$end."' AND machine_id IN (SELECT machine_id FROM `machines` WHERE active=1) ORDER BY time,machine_id"; 
       $result =  mysql_query($query);
       while($row = mysql_fetch_array($result)){
          $m_id = $row['machine_id'];
