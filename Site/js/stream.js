2c8e08e1bf67fd03031cbf775f39e54598cb2798
Reed La Botz <reedlabotz@gmail.com>
2012-02-03 18:38:00 -0600

    Basic working

--- Site/js/stream.js
+++ Site/js/stream.js
@@ -1,46 +1,182 @@
-var width = 960;
-var height = 400;
-
-d3.json('data.php', function(json){
-   var minX = d3.min(json,function(d){ return d3.min(d,function(d){ return d.x; }); });
-   var maxX = d3.max(json,function(d){ return d3.max(d,function(d){ return d.x; }); });
-   var maxY = d3.max(json,function(d){ return d3.max(d,function(d){ return d.y }); });
+(function(){
+   var vars = [];
+   var currentVar = null;
+   var currentStartTime = null;
+   var currentEndTime = null;
+   var hasDrawn = false;
    
-   var data0 = d3.layout.stack().offset("silhouette")(json);
-
-   var x = d3.time.scale().domain([new Date(minX), new Date(maxX)]).range([0, width]);
-
-   var y = d3.scale.linear().domain([0, maxY]).range([0, height/4]);
-
+   $(function(){
+      //prevent the textboxs in the nav bar to close the dropdown menus
+      $('.dropdown-menu input[type="text"]').click(function (e) {
+      	e.stopPropagation();
+    	});
+      
+      $("#start_done").click(function(e){
+         setStartAndEndTime();
+      });
+      
+      $("#end_done").click(function(e){
+         setStartAndEndTime();
+      });
+      
+      $("#var_choices").click(function(e){
+         var id = e.srcElement.id;
+         setVar(id.substring(4,id.length));
+      });
+      
+      $("#display_btn").click(function(){
+         drawGraph();
+         window.location.hash = currentVar;
+      });
+      
+      $(window).resize(function(){
+         drawGraph();
+      });
+         	
+   	init();
+   });
    
-   var vis = d3.select("#chart").append("svg").attr("width",width).attr("height",height);
+   function init(){
+      $.getJSON('json.php?request=vars',function(json){
+         if(json['error']){
+            throwError(json['error']);
+         }
+         vars = json;
+         //set the end time to now
+         currentEndTime = new Date();  
+         //set the start time to yesturday
+         currentStartTime = new Date();
+         currentStartTime.setDate(currentStartTime.getDate()-1);
+         
+         //set the vars dropdown
+         for (v in vars){
+            $("#var_choices").append("<li><a id='var-"+v+"'>" + vars[v] + "</a></li>");
+         }
+         
+         var hash = window.location.hash;
+         setVar(hash.substr(1,hash.length-1));
+         
+         drawStartAndEndTime();
+         
+         drawGraph();
+      });
+   };
    
-   var rules = vis.selectAll("g.rule")
-       .data(x.ticks(12))
-     .enter().append("g")
-       .attr("class", "rule");
-
-   rules.append("line")
-       .attr("x1", x)
-       .attr("x2", x)
-       .attr("y1", 0)
-       .attr("y2", height - 1);
-
-   rules.append("text")
-       .attr("x", x)
-       .attr("y", height - 13)
-       .attr("dy", ".71em")
-       .attr("text-anchor", "middle")
-       .text(x.tickFormat(12));
+   function drawGraph(){
+      if(hasDrawn){
+         $("#chart").html("");
+      }
+      d3.json('json.php?request=data&var='+currentVar+'&start='+currentStartTime.getTime()+'&end='+currentEndTime.getTime(), function(json){
+         if(json['error']){
+            throwError(json['error']);
+         }else{
+           innerDraw(json);
+         }
+      });
+   }
    
-   var area = d3.svg.area()
-                    .x(function(d){ return x(new Date(d.x)); })
-                    .y0(function(d){ return y(d.y0); })
-                    .y1(function(d){ return y(d.y + d.y0); });
+   function innerDraw(json){
+      var width = $(document).width();
+      var height = $(document).height() - 58; //58 for header bar
+      
+      var minX = d3.min(json,function(d){ return d3.min(d,function(d){ return d.x; }); });
+      var maxX = d3.max(json,function(d){ return d3.max(d,function(d){ return d.x; }); });
+      var maxY = d3.max(json,function(d){ return d3.max(d,function(d){ return d.y }); });
 
-   var color = d3.interpolateRgb("#aad", "#556");
+      currentStartTime = new Date(minX);
+      currentEndTime = new Date(maxX);
+      drawStartAndEndTime();
+      
 
-   var i = 0;
-   vis.selectAll("path").data(data0).enter().append("path").style("fill",function(){ return color(i++/4); }).attr("d",area);
+      var data0 = d3.layout.stack().offset("silhouette")(json);
+
+      var x = d3.time.scale().domain([new Date(minX), new Date(maxX)]).range([0, width]);
+
+      var y = d3.scale.linear().domain([0, maxY]).range([0, height/4]);
+
+
+      var vis = d3.select("#chart").append("svg").attr("width",width).attr("height",height);
+
+      var rules = vis.selectAll("g.rule")
+          .data(x.ticks(12))
+        .enter().append("g")
+          .attr("class", "rule");
+
+      rules.append("line")
+          .attr("x1", x)
+          .attr("x2", x)
+          .attr("y1", 0)
+          .attr("y2", height - 1);
+
+      rules.append("text")
+          .attr("x", x)
+          .attr("y", height - 13)
+          .attr("dy", ".71em")
+          .attr("text-anchor", "middle")
+          .text(x.tickFormat(12));
+
+      var area = d3.svg.area()
+                       .x(function(d){ return x(new Date(d.x)); })
+                       .y0(function(d){ return y(d.y0); })
+                       .y1(function(d){ return y(d.y + d.y0); });
+
+      var color = d3.interpolateRgb("#aad", "#556");
+
+      var i = 0;
+      vis.selectAll("path").data(data0).enter().append("path").style("fill",function(){ return color(i++/4); }).attr("d",area);
+      hasDrawn = true;
+   }
    
-  });
+   function drawStartAndEndTime(){
+      $("#start_text span").html(formatDateTime(currentStartTime));
+      $("#start_date").val(formatDate(currentStartTime));
+      $("#start_time").val(formatTime(currentStartTime));
+      var start_datepicker = $("#start_date").data('datepicker');
+      start_datepicker.selectDate(currentStartTime);
+      
+      $("#end_text span").html(formatDateTime(currentEndTime));
+      $("#end_date").val(formatDate(currentEndTime));
+      $("#end_time").val(formatTime(currentEndTime));
+      var end_datepicker = $("#end_date").data('datepicker');
+      end_datepicker.selectDate(currentEndTime);
+   }
+   
+   function setStartAndEndTime(){
+      var end_date = $("#end_date").data('datepicker').selectedDate;
+      var end_time = Date.parse($("#end_time").val());
+      currentEndTime = new Date(end_date.getFullYear(),end_date.getMonth(),end_date.getDate(),end_time.getHours(),end_time.getMinutes());
+      
+      var start_date = $("#start_date").data('datepicker').selectedDate;
+      var start_time = Date.parse($("#start_time").val());
+      currentStartTime = new Date(start_date.getFullYear(),start_date.getMonth(),start_date.getDate(),start_time.getHours(),start_time.getMinutes());
+      drawStartAndEndTime();
+   }
+   
+   function setVar(id){
+      if(vars[id]){
+         currentVar = id;
+      }else{
+         //set the current var to the first var
+         for (first in vars) break;
+         currentVar = first;
+      }
+      $("#var_choices a").removeClass('active');
+      $("#var-"+currentVar).addClass('active');
+      $("#var_text span").html(vars[currentVar]);
+   }
+   
+   function formatDateTime(date) {
+      var format = d3.time.format("%x %I:%M %p");
+      return format(date);
+   }
+   
+   function formatDate(date){
+      var format = d3.time.format("%x");
+      return format(date);
+   }
+   
+   function formatTime(date){
+      var format = d3.time.format("%I:%M %p");
+      return format(date);
+   }
+})();
---
+++ Site/json.php
@@ -0,0 +1,59 @@
+<?php
+date_default_timezone_set('America/Chicago');
+$dbname = 'labotz1_EWSGraph';
+$dbuser = 'labotz1_databot';
+$dbpass = 'Me^G8eXD?=!v';
+$host = 'dcs-projects.cs.illinois.edu';
+
+$variables = array("user_count" => "User Count",
+                   "task_count" => "Task Count",
+                   "cpu_user" => "CPU User",
+                   "cpu_system" => "CPU System",
+                   "mem_used" => "Memory Used",
+                   "mem_free" => "Memory Free");
+
+$dbconn = mysql_connect($host, $dbuser, $dbpass) or die (json_encode(array("error" => "Could not connect to database")));
+mysql_select_db($dbname);
+
+$data = array();
+
+$request = mysql_real_escape_string($_GET['request']);
+
+if($request == "data"){
+   $var = mysql_real_escape_string($_GET['var']);
+   if(!array_key_exists($var,$variables)){
+      $data['error'] = "Invalid variable";
+   }else{
+      $start = date("Y-m-d H:i:s",mysql_real_escape_string($_GET['start'])/1000);
+      $end = date("Y-m-d H:i:s",mysql_real_escape_string($_GET['end'])/1000);
+   	$machines = array();
+   	$query = "SELECT machine_id,time,".$var." FROM `snapshots` JOIN `machines` ON `snapshots`.machine_id=`machines`.id ";
+   	$query .="WHERE `machines`.active=1 AND `snapshots`.time >= '".$start."' AND `snapshots`.time <= '".$end."'"; 
+   	$result =  mysql_query($query);
+   	while($row = mysql_fetch_array($result)){
+   	   $m_id = $row['machine_id'];
+   	   if(!array_key_exists($m_id,$machines)){
+   	      $machines[$m_id] = sizeof($data);
+   	      array_push($data,array());
+   	   }
+   	   array_push($data[$machines[$m_id]],array('x' => strtotime($row['time'])*1000, 'y' => floatval($row[$var])));
+   	}
+   }
+}else if($request == "vars"){
+   $data = $variables;
+}else if($request == "machines"){
+   $query = "SELECT id,name FROM `machines` WHERE active=1";
+   $result = mysql_query($query);
+   while($row = mysql_fetch_array($result)){
+      $data[$row['id']] = $row['name'];
+   }
+}else{
+	$data['error'] = "Invalid request type";
+}
+
+mysql_close($dbconn);
+
+$json = json_encode($data);
+
+echo($json);
+?>
